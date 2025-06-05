454d5a6be7fe4f624845231771ee3ac781401d47
taichyy <chinzeilulu@gmail.com>
2025-06-05 11:32:07 +0800

    New: Logout dialog.

--- components/buttons/button-logout.tsx
+++ components/buttons/button-logout.tsx
@@ -2,9 +2,13 @@
 import { useRouter } from "next/navigation";
 
 import { Button } from "../ui/button";
+import DialogDoubleCheck from "../dialog-double-check";
+import { useDoubleCheckStore } from "@/lib/stores/use-double-check-store";
 
 const ButtonLogout = () => {
     const router = useRouter()
+
+    const { setDoubleCheckOpen } = useDoubleCheckStore();
     
     const handleClick = async () => {
         // remove cookie server-side
@@ -12,9 +16,17 @@ const ButtonLogout = () => {
         router.refresh()
     };
     return (
-        <Button onClick={() => handleClick()}>
-            登出
-        </Button>
+        <>
+            <Button onClick={() => setDoubleCheckOpen("logout", true)}>
+                確定登出
+            </Button>
+            <DialogDoubleCheck
+                id="logout"
+                title="確定登出"
+                desc="請務必確實記下您的登入密碼。如果重設密碼，將會清除所有資料。"
+                doFunction={handleClick}
+            />
+        </>
     );
 }
 
