c5b7fb809526104c6a5543cd1e7f79d427350c10
taichyy <chinzeilulu@gmail.com>
2025-08-11 00:23:36 +0800

    New: Temp email route.

---
+++ app/api/email/route.js
@@ -0,0 +1,79 @@
+const nodemailer = require('nodemailer');
+
+import { Response } from "@/lib/utils"
+
+export async function POST(request) {
+    const body = await request.json();
+
+    const { email, username, content } = body || {}
+
+    const { setStatus, setResponse, getResponse } = Response()
+
+    if (!email) {
+        setStatus(400)
+        setResponse({
+            status: false,
+            message: "Email is required.",
+        })
+
+        return getResponse();
+    }
+
+    if (!content) {
+        setStatus(400)
+        setResponse({
+            status: false,
+            message: "Content is required.",
+        })
+
+        return getResponse();
+    }
+
+    const message = {
+        from: `TaidyPass Team <${process.env.EMAIL_FROM}>`,
+        to: email,
+        subject: "Using NodeMailer with Next.js API.",
+        html: `
+            <p>
+                Hello ${username, content || "親愛的用戶"},
+            </p>
+
+            ${content}
+        `,
+        headers: {
+            "X-Entity-Ref-ID": "newmail",
+        }
+    }
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_FROM,
+            pass: process.env.EMAIL_PASS,
+        },
+        tls: {
+            // Do not fail on invalid certs
+            rejectUnauthorized: false
+        }
+    })
+
+    try {
+        await transporter.sendMail(message);
+
+        setStatus(200)
+        setResponse({
+            status: true,
+            message: "Email sent successfully.",
+        })
+    } catch (error) {
+        console.error("Error sending email:", error);
+
+        setStatus(500)
+        setResponse({
+            status: false,
+            message: "Email sending failed.",
+        })
+    } finally {
+        return getResponse();
+    }
+}
