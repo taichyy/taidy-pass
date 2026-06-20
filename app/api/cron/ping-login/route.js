40d3ac4ed36b3c60f0f2969d1e8717506856d749
taichyy <chinzeilulu@gmail.com>
2026-06-21 01:30:17 +0800

    Fix: Use correct env.

--- app/api/cron/ping-login/route.js
+++ app/api/cron/ping-login/route.js
@@ -10,7 +10,7 @@ export const GET = async (request) => {
         });
     }
 
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 
     try {
         const response = await fetch(`${baseUrl}/api/session`, {
